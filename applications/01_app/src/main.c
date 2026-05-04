dbb86164a58bebde29ca82e2b15fb2ccdffbb98c
Mondher Mechri <monder.mechri@isimg.tn>
2026-05-04 07:16:54 +0000

    Refactor main function return type to int for consistency and error handling

--- applications/01_app/src/main.c
+++ applications/01_app/src/main.c
@@ -7,24 +7,24 @@
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-void main(void)
+int main(void)
 {
     int ret;
 
     if (!gpio_is_ready_dt(&led)) {
         printk("Error: LED device is not ready\n");
-        return;
+        return 0;
     }
 
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
-        return;
+        return 0;
     }
 
     while (1) {
         ret = gpio_pin_toggle_dt(&led);
         if (ret < 0) {
-            return;
+            return 0;
         }
         printk("LED Toggled\n");
         k_msleep(100);
